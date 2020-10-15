469855deeea76de2004c34619f950bd74ffd8898
wpp <wppbox@163.com>
2020-10-15 22:01:22 +0800

    'test60'

--- routes/index.js
+++ routes/index.js
@@ -6,62 +6,49 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get(`/sh/:id`, function (req, res) {
-  const { id } = req.params;
-  // branch 必填 
-  let allObj = {
-    "test": {branch:'main',shell:'restart'},
-  }
-  if(allObj[id]){
+router.get(`/log`, function (req, res) {
+  
     res.send({
       code: 0,
-      data: "正在更新，详细日志访问",
-      msg: "收到提示",
+      data: "暂未开发",
+      msg: "提示",
     });
-  }else{
-    res.send({
-      code: 404,
-      data: "没有 "+id+" 的链接",
-      msg: "收到提示",
-    });
-    return;
-  }
-  // 子进程
-  var exec = require("child_process").exec; //需要执行的命令字符串
-  var cli = "sh ./shell/" + id + ".sh"; // 执行命令
-  exec(cli, { encoding: "utf8" }, function (err, stdout, stderr) {
-    if(err){ // 如果抛出错误
-
-      return
-    }
-    // 如果没有错误,则执行命令成功
-
-  })
+  
 });
 
 // post请求
 router.post(`/sh/:id`, function (req, res) {
   const { id } = req.params;
-  // branch 必填 
+  /**
+   * branch 必填分支
+   * shell  选填脚本
+   * */ 
   let allObj = {
     "test": {branch:'main',shell:'restart'},
   }
-  console.log(49)
-  console.log(req.body.payload)
-  console.log(JSON.parse(req.body.payload))
-  console.log(req.body.payload.ref)
-  console.log(JSON.parse(req.body.payload).ref)
+  let ref = JSON.parse(req.body.payload).ref
+  let refArr = ref.split('/')
+
   if(allObj[id]){
-    res.send({
-      code: 0,
-      data: "正在更新中，详细日志请访问http://localhost/log",
-      msg: "收到提示",
-    });
+    if(refArr[refArr.length-1] === allObj[id].branch){
+      res.send({
+        code: 0,
+        data: "正在更新中，详细日志请访问 http://sh.wpp47.top/log",
+        msg: "提示",
+      });
+    }else{
+      res.send({
+        code: 603,
+        data: "仅收到"+allObj[id].branch+"分支的提示才会下一步",
+        msg: "提示",
+      });
+      return;
+    }
   }else{
     res.send({
       code: 404,
       data: "没有 "+id+" 的链接",
-      msg: "收到提示",
+      msg: "提示",
     });
     return;
   }
