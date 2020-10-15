f54fdd5b5b4e7fef21edc234b458568d5846f6eb
notebook <wpp@notebook.com>
2020-10-15 16:13:09 +0800

    'post1'

--- routes/index.js
+++ routes/index.js
@@ -36,5 +36,34 @@ router.get(`/sh/:id`, function (req, res) {
     });
   });
 });
+router.post(`/sh/test`, function (req, res) {
+  const id = test;
+  // 子进程
+  var exec = require("child_process").exec; //需要执行的命令字符串
+  var cli = "sh ./shell/" + id + ".sh"; // 执行命令
+  exec(cli, { encoding: "utf8" }, function (err, stdout, stderr) {
+    // 如果抛出错误
+    if (err) {
+      console.log(`err`, err); // 返回结果
+      res.send({
+        code: -1,
+        data: `一些错误`,
+        msg: err,
+      });
+      return;
+    } // 如果没有错误,则执行命令成功
+    console.log("stdout >>>>>>>>>>>>> " + stdout);
+    console.log("stderr >>>>>>>>>>>>> " + stderr); // 返回结果
+    res.send({
+      code: 0,
+      data: `更新成功`,
+      msg: `
+             ${stdout}
+             >>>>>
+             ${stderr}
+             `,
+    });
+  });
+});
 
 module.exports = router;
