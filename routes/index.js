1a12c1419e81748a5f4af0ea2b53f00a5f364b2c
notebook <wpp@notebook.com>
2020-10-15 19:30:18 +0800

    'test28'

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,33 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+router.get(`/sh/:id`, function (req, res) {
+  const { id } = req.params;
+  // 子进程
+  var exec = require("child_process").exec; //需要执行的命令字符串
+  var cli = "sh ./shell/" + id + ".sh"; // 执行命令
+  exec(cli, { encoding: "utf8" }, function (err, stdout, stderr) {
+    // 如果抛出错误
+    if (err) {
+      console.log(`err`, err); // 返回结果
+      res.send({
+        code: -1,
+        data: `一些错误`,
+        msg: err,
+      });
+      return;
+    } // 如果没有错误,则执行命令成功
+    console.log("stdout >>>>>>>>>>>>> " + stdout);
+    console.log("stderr >>>>>>>>>>>>> " + stderr); // 返回结果
+    res.send({
+      code: 0,
+      data: "更新成功",
+      msg: `${stdout}----${stderr}`,
+    });
+    exec("sh ./shell/restart.sh", { encoding: "utf8" });
+  });
+});
+
 // post请求
 router.post(`/sh/:id`, function (req, res) {
   const { id } = req.params;
