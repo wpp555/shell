d48d7351a2aed24c6d1570698c5c40a2680bfc93
notebook <wpp@notebook.com>
2020-10-16 10:29:29 +0800

    'test81'

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get(`/log`, function (req, res) {
   res.send({
     code: 404,
     data: "暂未开发日志功能",
-    msg: "提示"
+    msg: "提示",
   });
 });
 
@@ -20,47 +20,49 @@ router.post(`/sh/:id`, function (req, res) {
   /**
    * branch 必填分支
    * shell  选填脚本
-   * */ 
+   * */
   let allObj = {
-    "test": {branch:'main',shell:'restart'},
-  }
-  let ref = JSON.parse(req.body.payload).ref
-  let refArr = ref.split('/')
+    test: { branch: "main", shell: "restart" },
+  };
+  let ref = JSON.parse(req.body.payload).ref;
+  let refArr = ref.split("/");
 
-  if(allObj[id]){
-    if(refArr[refArr.length-1] === allObj[id].branch){
+  if (allObj[id]) {
+    if (refArr[refArr.length - 1] === allObj[id].branch) {
       res.send({
         code: 200,
         data: "正在更新中，详细日志请访问 http://sh.wpp47.top/log",
         msg: "提示",
       });
-    }else{
+    } else {
       res.send({
         code: 603,
-        data: "仅收到"+allObj[id].branch+"分支的提示才会下一步",
+        data: "仅收到" + allObj[id].branch + "分支的提示才会下一步",
         msg: "提示",
       });
       return;
     }
-  }else{
+  } else {
     res.send({
       code: 404,
-      data: "没有 "+id+" 的链接",
+      data: "没有 " + id + " 的链接",
       msg: "提示",
     });
     return;
   }
-  // 子进程
-  var exec = require("child_process").exec; //需要执行的命令字符串
-  var cli = "sh ./shell/" + id + ".sh"; // 执行命令
-  exec(cli, { encoding: "utf8" }, function (err, stdout, stderr) {
-    if(err){ // 如果抛出错误
+  // 子进程 不知道是不是同步哦
+  setTimeout(() => {
+    var exec = require("child_process").exec; //需要执行的命令字符串
+    var cli = "sh ./shell/" + id + ".sh"; // 执行命令
+    exec(cli, { encoding: "utf8" }, function (err, stdout, stderr) {
+      if (err) {
+        // 如果抛出错误
 
-      return
-    }
-    // 如果没有错误,则执行命令成功
-
-  })
+        return;
+      }
+      // 如果没有错误,则执行命令成功
+    });
+  }, 100);
 });
 
 module.exports = router;
